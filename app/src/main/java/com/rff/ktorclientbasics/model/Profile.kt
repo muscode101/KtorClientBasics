068487204ea453930e33f76a00b1617e3742a402
julius chibaye <46082663+muscode101@users.noreply.github.com>
2022-09-20 10:11:50 +0200

    added SerializedNames to profile model to get all fields

--- app/src/main/java/com/rff/ktorclientbasics/model/Profile.kt
+++ app/src/main/java/com/rff/ktorclientbasics/model/Profile.kt
@@ -1,18 +1,20 @@
 package com.rff.ktorclientbasics.model
 
-
+import com.google.gson.annotations.SerializedName
 data class Profile(
-    val id: Int,
-    val screenTimeout: Int,
-    val country: String,
-    val appTitle: String,
-    val account: String,
-    val appMessage: String,
-    val shotMessage: String,
-    val fullMessage: String,
-    val navColor: String,
-    val backgroundColor: String,
-    val backgroundUrl: String,
-    val isExitAllowed: Boolean,
-    val allowedApps: List<ELKApp>,
+    @SerializedName("id"               ) var id              : Int?              = null,
+    @SerializedName("screen_timeout"   ) var screenTimeout   : Int?              = null,
+    @SerializedName("country"          ) var country         : String?           = null,
+    @SerializedName("app_title"        ) var appTitle        : String?           = null,
+    @SerializedName("account"          ) var account         : String?           = null,
+    @SerializedName("app_message"      ) var appMessage      : String?           = null,
+    @SerializedName("short_message"    ) var shortMessage    : String?           = null,
+    @SerializedName("full_message"     ) var fullMessage     : String?           = null,
+    @SerializedName("nav_color"        ) var navColor        : String?           = null,
+    @SerializedName("background_color" ) var backgroundColor : String?           = null,
+    @SerializedName("background_url"   ) var backgroundUrl   : String?           = null,
+    @SerializedName("is_exit_allowed"  ) var isExitAllowed   : Int?              = null,
+    @SerializedName("allowed_apps"     ) var allowedApps     : List<ELKApp> = arrayListOf(),
+    @SerializedName("created"          ) var created         : String?           = null,
+    @SerializedName("modified"         ) var modified        : String?           = null
 )
